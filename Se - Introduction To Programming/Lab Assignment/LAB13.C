d3f288c93259d04b2ae35c30e86273ca3886f050
karanpatel79 <kp480749@gmail.com>
2025-08-09 18:03:13 +0530

    09-08-2025

--- Se - Introduction To Programming/Lab Assignment/LAB13.C
+++ Se - Introduction To Programming/Lab Assignment/LAB13.C
@@ -1,4 +1,4 @@
-/*Write a C program that acts as a simple calculator. The program should take two numbers
+/* Write a C program that acts as a simple calculator. The program should take two numbers
 and an operator as input from the user and perform the respective operation (addition,
 subtraction, multiplication, division, or modulus) using operators.
 
